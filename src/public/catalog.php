7d7129bb2ec763c7031b41c904c0230f39cbc92b
user <tuchinov99@bk.ru>
2026-09-22 01:57:29 +0800

    catalog

---
+++ src/public/catalog.php
@@ -0,0 +1,7 @@
+<?php
+
+$pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname=mydb', 'user', 'pass');
+$stmt = $pdo->query("SELECT * FROM products");
+$products = $stmt->fetchAll();
+
+require_once './catalog_page.php';
