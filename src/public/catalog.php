5050442b271528f27c9e7c387e717479865bed62
user <tuchinov99@bk.ru>
2026-09-26 00:03:27 +0800

    Добавляет редирект и COOKIE

--- src/public/catalog.php
+++ src/public/catalog.php
@@ -1,5 +1,10 @@
 <?php
 
+if (!isset($_COOKIE['user_id'])) {
+    header('Location: login_form.php');
+    exit;
+}
+
 $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname=mydb', 'user', 'pass');
 $stmt = $pdo->query("SELECT * FROM products");
 $products = $stmt->fetchAll();
